8f19c9651f3e9d5f63cbc8e6101e744af6e069a9
Serge Labada <sergelabada@gmail.com>
2022-08-04 19:25:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson05androidfragment/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.lesson05androidfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lesson05androidfragment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.container1, CounterFragment())
+            .add(R.id.container2, CounterFragment())
+            .commit()
+    }
+}
